bd03636f6f577808e26b75e77c37deee331bb30f
yang <wuzhikun526@163.com>
2022-12-19 23:23:57 +0800

    feat：组建区域搭建

--- src/components/config.js
+++ src/components/config.js
@@ -1,13 +1,12 @@
 // 组件基本配置
-export const componentConfig = [
+export const componentConfigs = [
   {
+    id: 0,
     // 自定义配置
     __config__: {
       label: '单行文本',
       labelWidth: null,
-      showLabel: true,
       tag: 'el-input',
-      tagIcon: 'input',
       defaultValue: undefined,
       required: true,
       layout: 'ColFormItem',
@@ -22,10 +21,104 @@ export const componentConfig = [
     placeholder: '请输入',
     style: { width: '100%' },
     clearable: true,
-    'prefix-icon': '',
-    'suffix-icon': '',
-    maxlength: null,
     readonly: false,
     disabled: false
-  }
+  },
+  {
+    id: 1,
+    __config__: {
+      label: '多行文本',
+      labelWidth: null,
+      tag: 'el-input',
+      defaultValue: undefined,
+      required: true,
+      layout: 'ColFormItem',
+      span: 24,
+    },
+    type: 'textarea',
+    placeholder: '请输入',
+    autosize: {
+      minRows: 2,
+      maxRows: 4
+    },
+    style: { width: '100%' },
+    readonly: false,
+    disabled: false
+  },
+  {
+    id: 2,
+    __config__: {
+      label: '下拉选择',
+      showLabel: true,
+      labelWidth: null,
+      tag: 'el-select',
+      required: true,
+      layout: 'ColFormItem',
+      span: 24,
+    },
+    __slot__: {
+      options: [{
+        label: '选项一',
+        value: 1
+      }, {
+        label: '选项二',
+        value: 2
+      }]
+    },
+    placeholder: '请选择',
+    style: { width: '100%' },
+    clearable: true,
+    disabled: false,
+  },
+  {
+    id: 3,
+    __config__: {
+      label: '单选框组',
+      labelWidth: null,
+      tag: 'el-radio-group',
+      changeTag: true,
+      defaultValue: undefined,
+      layout: 'ColFormItem',
+      span: 24,
+      optionType: 'default',
+      required: true,
+      border: false,
+    },
+    __slot__: {
+      options: [{
+        label: '选项一',
+        value: 1
+      }, {
+        label: '选项二',
+        value: 2
+      }]
+    },
+    style: {},
+    disabled: false
+  },
+  {
+    id: 4,
+    __config__: {
+      label: '多选框组',
+      tag: 'el-checkbox-group',
+      defaultValue: [],
+      span: 24,
+      labelWidth: null,
+      layout: 'ColFormItem',
+      optionType: 'default',
+      required: true,
+      border: false,
+    },
+    __slot__: {
+      options: [{
+        label: '选项一',
+        value: 1
+      }, {
+        label: '选项二',
+        value: 2
+      }]
+    },
+    style: {},
+    disabled: false
+  },
 ]
--- src/view/MainEditor.vue
+++ src/view/MainEditor.vue
@@ -2,14 +2,36 @@
   <div class="container">
     <!-- 组件列表 -->
     <div class="left-board">
-  
+      <draggable
+        :list="componentList"
+        :group="{ name: 'componentsGroup', pull: 'clone', put: false }"
+        @start="onStart"
+        @end="onEnd"
+        item-key="id">
+        <template #item="{ element }">
+          <div class="component-body">{{ element.__config__.label }}</div>
+        </template>
+      </draggable>
     </div>
     <!-- 展示区域 -->
     <div class="center-board">
-  
+      
     </div>
   </div>
 </template>
+
+<script setup>
+import draggable from 'vuedraggable'
+import { reactive } from 'vue'
+import { componentConfigs } from '../components/config'
+const componentList = reactive(componentConfigs)
+const onStart = (e) => {
+  console.log('拖动开始', e)
+}
+const onEnd = (e) => {
+  console.log('拖动结束', e)
+}
+</script>
 
 <style>
 .left-board {
@@ -21,5 +43,17 @@
   width: 100%;
   margin: 0 350px 0 260px;
   box-sizing: border-box;
+}
+.component-body {
+  width: 120px;
+  height: 30px;
+  border: 1px solid #000;
+  border-radius: 4px;
+  text-align: center;
+  line-height: 30px;
+  margin: 10px 10px 0;
+}
+.component-body:hover {
+  cursor: move;
 }
 </style>
